3f6874bd810657ca1d85f1dd207e9db502a08eff
jfuentes-gapsi <jfuentes@grupoasesores.com.mx>
2025-10-31 18:44:29 -0600

    JFUENTES
    jfuentes

---
+++ src/heroes/components/HeroList.jsx
@@ -0,0 +1,26 @@
+import { useMemo } from 'react';
+import { getHeroesByPublisher } from '../helpers/getHeroesByPublisher';
+import { HeroCard } from './HeroCard';
+
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = useMemo(() =>
+        getHeroesByPublisher(publisher),
+        [publisher]
+    );
+
+    return (
+        <div className='row rows-cols-1 row-cols-md-3 g-3'>
+            {
+                heroes.map((hero) => (
+                    <div
+                        key={hero.id}
+                    >
+                        <HeroCard {...hero} />
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
